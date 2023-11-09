9c47cdcbc2a8ef5f77005c42a0bf30c0af20c990
James Hackett <james.hackett5@mail.dcu.ie>
2023-11-09 05:52:43 +0000

    Add deployment scripts for canary and review environments

---
+++ src/api/tools/deploy-review.sh
@@ -0,0 +1,10 @@
+#!/usr/local/bin/bash
+
+DEPLOY_URL="${CI_ENVIRONMENT_SLUG}.prospector.ie"
+echo "Deploy URL: ${DEPLOY_URL}"
+levant deploy \
+  -var git_sha="${CI_COMMIT_SHORT_SHA}" \
+  -var environment_slug="${CI_ENVIRONMENT_SLUG}" \
+  -var deploy_url="${DEPLOY_URL}" \
+  -address "http://nomad.service.consul:4646" \
+  tools/templates/api-review.hcl
