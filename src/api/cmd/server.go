8695fc9f0451273508c10cf2833c46c96476b496
James Hackett <james.hackett5@mail.dcu.ie>
2024-01-01 20:39:17 +0000

    32: Simplify API code. Introduce nice UI for terminal based clients.

---
+++ src/api/cmd/server.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"prospector/controllers"
+	"prospector/routes"
+
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/cobra"
+)
+
+// serverCmd represents the server command
+var serverCmd = &cobra.Command{
+	Use:   "server",
+	Short: "Start the Prospector API server",
+	Long:  `Starts the Prospector API server. The server will be bound to the port specified by the --port flag.`,
+
+	Run: func(cmd *cobra.Command, args []string) {
+		r := gin.Default()
+
+		c := controllers.NewController()
+
+		routes.CreateRoutes(r, c)
+
+		r.Run(":" + cmd.Flag("port").Value.String())
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(serverCmd)
+
+	serverCmd.Flags().StringP("port", "p", "3434", "Port to bind the server to")
+}
--- src/api/main.go
+++ src/api/main.go
@@ -1,41 +1,14 @@
 package main
 
-import (
-	"fmt"
-	"os"
-	"prospector/command"
+import "prospector/cmd"
 
-	"github.com/mitchellh/cli"
-)
+//	@title			Prospector API
+//	@description	API backend for Prospector
+//	@version		1.0
+//	@host			prospector.ie
+//	@schemes		https
+//	@BasePath		/api
 
-// pass all cli args to the base of commands
 func main() {
-	os.Exit(Run(os.Args[1:]))
-}
-
-func Run(args []string) int {
-	metaPtr := new(command.Meta)
-
-	agentUi := &cli.BasicUi{
-		Reader:      os.Stdin,
-		Writer:      os.Stdout,
-		ErrorWriter: os.Stderr,
-	}
-
-	commands := command.Commands(metaPtr, agentUi)
-
-	cli := &cli.CLI{
-		Name:     "prospector",
-		Args:     args,
-		Commands: commands,
-	}
-
-	exitCode, err := cli.Run()
-
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error executing command: %s\n", err.Error())
-		return 1
-	}
-
-	return exitCode
+	cmd.Execute()
 }
