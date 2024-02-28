16f2f9ae22681611c6f0edbd6024a0c325d0cc99
James Hackett <james.hackett5@mail.dcu.ie>
2024-02-28 02:06:19 +0000

    52: Add TUI commands for managing jobs via the CLI

---
+++ src/api/cmd/stop.go
@@ -0,0 +1,76 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/spf13/cobra"
+)
+
+// stopCmd represents the stop command
+var stopCmd = &cobra.Command{
+	Use:   "stop",
+	Short: "Stop a job",
+	Long: `Stops a job with the given name. If the --purge flag is set, the job will be purged from the system.
+	
+For example:
+	prospect job stop my-job
+	prospect job stop my-job --purge`,
+	Run: func(cmd *cobra.Command, args []string) {
+		name := args[0]
+		purge, _ := cmd.Flags().GetBool("purge")
+
+		purgeText := "false"
+		if purge {
+			purgeText = "true"
+		}
+
+		healthURL := "http://localhost:3434/api/health"
+
+		// make the request to the server
+		res, err := http.Get(healthURL)
+		if err != nil {
+			fmt.Println("Error: Could not connect to prospector server, is it running?")
+			return
+		}
+
+		// check the status code of the response
+		if res.StatusCode != 200 {
+			fmt.Println("Error: The server returned an error. Please try again.")
+			return
+		}
+
+		jobStopUrl := "http://localhost:3434/api/jobs/" + name + "?purge=" + purgeText
+
+		// make the request to the server
+		req, err := http.NewRequest(http.MethodDelete, jobStopUrl, nil)
+		if err != nil {
+			fmt.Println("Error: The server responded with an error. Please try again.")
+			return
+		}
+
+		res, err = http.DefaultClient.Do(req)
+		if err != nil {
+			fmt.Println("Error: The server responded with an error. Please try again.")
+			return
+		}
+
+		// check the status code of the response
+		if res.StatusCode != 200 {
+			fmt.Println("Error: The server returned an error. Please try again.")
+			return
+		}
+
+		fmt.Println("Job stopped successfully")
+	},
+}
+
+func init() {
+	jobCmd.AddCommand(stopCmd)
+
+	stopCmd.Args = cobra.ExactArgs(1)
+	stopCmd.Flags().BoolP("purge", "p", false, "Purge the job from the system")
+}
