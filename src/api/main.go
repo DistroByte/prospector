47979f1057939805af48444342436f55756a988c
James Hackett <james.hackett5@mail.dcu.ie>
2024-02-29 02:05:05 +0000

    Fix docs generation

--- src/api/main.go
+++ src/api/main.go
@@ -2,14 +2,6 @@ package main
 
 import "prospector/cmd"
 
-//	@title						Prospector API
-//	@description				API backend for Prospector
-//	@version					1.0
-//	@host						prospector.ie
-//	@schemes					https
-//	@BasePath					/api
-//	@securityDefinitions.basic	BasicAuth
-
 func main() {
 	cmd.Execute()
 }
