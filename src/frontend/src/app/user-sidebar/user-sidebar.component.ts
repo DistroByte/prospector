e8f25192da091d39b8c20f1d57f5bc74c87dd9fa
Alexandru Dorofte <alexandru.dorofte2@mail.dcu.ie>
2024-04-20 23:48:33 +0000

    Sidebar Style Change

--- src/frontend/src/app/user-sidebar/user-sidebar.component.ts
+++ src/frontend/src/app/user-sidebar/user-sidebar.component.ts
@@ -1,20 +1,21 @@
-import {Component} from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatIconModule} from '@angular/material/icon';
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { UserSidebarService } from '../usersidebar.service';
-import {MatListModule} from '@angular/material/list';
+import { MatListModule } from '@angular/material/list';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-user-sidebar',
   standalone: true,
-  imports: [MatListModule, MatSidenavModule, MatIconModule,MatButtonModule,RouterOutlet, RouterLink],
+  imports: [MatListModule, MatSidenavModule, MatIconModule, MatButtonModule, RouterOutlet, RouterLink],
   templateUrl: './user-sidebar.component.html',
   styleUrl: './user-sidebar.component.css'
 })
 export class UserSidebarComponent {
-  constructor(public sidebarService: UserSidebarService) {}
+  constructor(public sidebarService: UserSidebarService, private authService: AuthService) { }
 
   toggleSidebar() {
     this.sidebarService.toggle();
@@ -23,6 +24,9 @@ export class UserSidebarComponent {
   get isOpen() {
     return this.sidebarService.isOpen;
   }
-  
+
+  logOut(): void {
+    this.authService.logOut();
+  }
 
 }
